9d7fba3813a061b3eb1fb66927c869208ee6654e
Ruben Ricaurte <ricaurtef@gmail.com>
2024-03-05 05:55:25 -0300

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+    tags = {
+      Maintainer = "Ruben Ricaurte: rricaurte@netrixllc.com",
+      ManagedBy  = "Terraform",
+    }
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-example",
+  }
+}
