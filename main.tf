2b51f94fd4b22f1a09a18f738a6d6aefa8b076b3
Ruben Ricaurte <ricaurtef@gmail.com>
2024-03-12 07:26:45 -0300

    Extending the example.
    Locals, variables, outputs and string interpolation.

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 
   default_tags {
     tags = {
@@ -9,10 +9,54 @@ provider "aws" {
   }
 }
 
+data "aws_ami" "ubuntu_jammy" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical.
+  name_regex  = "^ubuntu/images/hvm-ssd/ubuntu-jammy.*-amd64-server.*"
+}
+
+variable "server_port" {
+  description = "The http port to listen to requests."
+  type        = number
+  default     = 8080
+}
+
+output "ec2_public_ip" {
+    description = "The public IP address of the web server."
+    value = aws_instance.example.public_ip
+}
+
+locals {
+  instance_name = random_pet.instance_name.id
+}
+
+resource "random_pet" "instance_name" {
+  length    = 3
+  separator = "-"
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+  ami                         = data.aws_ami.ubuntu_jammy.id
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.example-sg.id]
+  user_data_replace_on_change = true
+
+  user_data = templatefile("${path.cwd}/user_data.tftpl", {
+    server_port = var.server_port
+  })
+
   tags = {
-    Name = "terraform-example",
+    Name = local.instance_name,
+  }
+}
+
+resource "aws_security_group" "example-sg" {
+  name = "${local.instance_name}-sg"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
